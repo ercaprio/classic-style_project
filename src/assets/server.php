51eb5631962db06502ca5a273625e05853357e57
Erkin <ercaprio@gmail.com>
2023-11-13 22:12:00 +0600

    Working with data submission forms

--- src/assets/server.php
+++ src/assets/server.php
@@ -1,2 +1,14 @@
 <?php
-echo var_dump($_POST);
+$_POST = json_decode(file_get_contents("php://input"), true);
+
+// Создаем массив данных
+$data = array(
+    "user_name" => $_POST['user_name'],
+    "user_phone" => $_POST['user_phone']
+);
+
+// Устанавливаем заголовок для указания, что ответ будет в формате JSON
+header('Content-Type: application/json');
+
+// Выводим данные в формате JSON
+echo json_encode($data);
